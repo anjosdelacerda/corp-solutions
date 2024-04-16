f8a0dabd92124c977d650a82da71f413488e7655
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-04-16 14:15:40 -0300

    att card e add conteudo

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,55 +1,68 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-interface ICard {
-  image: string;
-  company: string;
-  description: string;
+interface IMedal {
   medal: string;
   medalColor: string;
   medalDescription: string;
 }
 
-const Card = ({
-  image,
-  company,
-  description,
-  medal,
-  medalColor,
-  medalDescription,
-}: ICard) => {
+interface IList {
+  image: string;
+  company: string;
+  description: string;
+  medals: IMedal[];
+}
+
+interface ICard {
+  list: IList[];
+}
+
+const Card = ({ list }: ICard) => {
   return (
-    <Box
-      width={"292px"}
-      sx={{
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-      }}
-      display={"flex"}
-      flexDirection={"column"}
-      alignItems={"center"}
-      justifyContent={"space-evenly"}
-      padding={"35px"}
-      gap={"35px"}
-    >
-      <img src={image} alt={company} />
-      <Typography variant="body2" gutterBottom>
-        {description}
-      </Typography>
-      <Box display={"flex"} gap={"10px"}>
-        <Box display={"flex"} flexDirection={"column"}>
-          <img src={medal} alt={medalColor} width={"80px"} height={"80px"} />
-          <Typography variant="overline" gutterBottom>
-            {medalDescription}
+    <>
+      {list.map((item, index) => (
+        <Box
+          width={"292px"}
+          minHeight={"440px"}
+          sx={{
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          padding={"35px"}
+          gap={"30px"}
+          key={index}
+        >
+          <img src={item.image} alt={item.company} />
+          <Typography variant="body2" gutterBottom>
+            {item.description}
           </Typography>
+          <Box display={"flex"} gap={"30px"}>
+            {item.medals.map((medal, medalIndex) => (
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                key={medalIndex}
+              >
+                <img
+                  src={medal.medal}
+                  alt={medal.medalColor}
+                  width={"80px"}
+                  height={"80px"}
+                />
+                <Typography variant="overline" gutterBottom>
+                  {medal.medalDescription}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
         </Box>
-        {/* <Box display={"flex"} flexDirection={"column"}>
-          <img src={medal} alt={medalColor} width={"80px"} height={"80px"} />
-          <Typography variant="overline" gutterBottom>
-            {medalDescription}
-          </Typography>
-        </Box> */}
-      </Box>
-    </Box>
+      ))}
+    </>
   );
 };
 
--- src/layout/sections/partners/partners.tsx
+++ src/layout/sections/partners/partners.tsx
@@ -1,13 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Card from "../../../components/card/card";
-import cocaCola from "../../../assets/logos/cocacola.png";
-import mcDonalds from "../../../assets/logos/mcdonalds.png";
-import dropBox from "../../../assets/logos/dropbox.png";
-import mitsubishi from "../../../assets/logos/mitsubishi.png";
-import instagram from "../../../assets/logos/instagram.png";
-import huawei from "../../../assets/logos/huawei.png";
-import diamondMedal from "../../../assets/icons/medal-diamond.png";
-import goldMedal from "../../../assets/icons/medal-gold.png";
+import { partnersList } from "./partnersList";
 
 const Partners = () => {
   return (
@@ -35,66 +28,7 @@ const Partners = () => {
           justifyContent={"center"}
           gap={"60px"}
         >
-          <Card
-            image={cocaCola}
-            company="Coca-Cola"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={diamondMedal}
-            medalColor="Medalha de diamante"
-            medalDescription="40% negros"
-          />
-          <Card
-            image={mcDonalds}
-            company="Mc Donalds"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={diamondMedal}
-            medalColor="Medalha de diamante"
-            medalDescription="30% LGBTs"
-          />
-          <Card
-            image={dropBox}
-            company="Dropbox"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={goldMedal}
-            medalColor="Medalha de ouro"
-            medalDescription="20% PCDS"
-          />
-          <Card
-            image={mitsubishi}
-            company="Mitsubishi"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={diamondMedal}
-            medalColor="Medalha de diamante"
-            medalDescription="40% negros"
-          />
-          <Card
-            image={instagram}
-            company="Instagram"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={goldMedal}
-            medalColor="Medalha de ouro"
-            medalDescription="20% PCDS"
-          />
-          <Card
-            image={huawei}
-            company="Huawei"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolores
-          amet similique odio incidunt cum iste a ad, vel adipisci illo hic
-          optio? Corporis alias expedita ipsum, tempore excepturi nostrum."
-            medal={diamondMedal}
-            medalColor="Medalha de diamante"
-            medalDescription="40% negros"
-          />
+          <Card list={partnersList} />
         </Box>
       </Box>
     </Box>
