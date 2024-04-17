a5e6a0e2b4211fa1d074a8fe30bbcdb5037ab61e
anjosdelacerda <anjosdelacerda@gmail.com>
2024-04-17 07:00:06 -0300

    :feat: adicionando validações com yup

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -4,10 +4,28 @@ import logoCorpSolutions from "../../assets/logos/corp-solutions.png";
 import { Link, useNavigate } from 'react-router-dom';
 import "./register.css"
 import {useForm} from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const Register = () => {
 
-  const {handleSubmit, register} = useForm()
+  const formSchema = yup.object().shape({
+    name: yup.string().required("nome é obrigatório"),
+    email: yup.string().email("e-mail inválido").required("e-mail obrigatório"),
+    birthday: yup.date().required("data de nascimento é obrigatória"),
+    password: yup
+    .string()
+    .required("Senha é obrigatória")
+    .min(6, "A senha deve ter pelo menos 8 caracteres"),
+    checkedPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'As senhas precisam ser iguais')
+    .required('Confirmação de senha é obrigatória')
+  })
+
+  const {handleSubmit, register, formState: {errors} } = useForm({
+    resolver: yupResolver(formSchema)
+  })
 
   const onSubmitFunction = (data: any) =>{
       console.log(data)
@@ -23,37 +41,38 @@ const Register = () => {
                     type={'text'}
                     register={register}
                     name={'name'}
-                    
                 />
+                {errors.name && errors.name.message}
                 <InputRegister
                     label={'E-mail'}
                     type={'email'}
                     register={register}
                     name={'email'}
                 />
+                {errors.email && errors.email.message}
 
                 <InputRegister
                     label={''}
                     type={'date'}
                     register={register}
                     name={'birthday'}
-                    
                 />
+                {errors.birthday && errors.birthday.message}
                 <InputRegister
                     label={'Senha'}
                     type={'password'}
                     register={register}
                     name={'password'}
-                    
                 />
+                {errors.password && errors.password.message}
                 
                 <InputRegister
                     label={'Confirme sua senha'}
                     type={'password'}
                     register={register}
                     name={'checkedPassword'}
-                    
                 />
+                {errors.checkedPassword && errors.checkedPassword.message}
                 <div className='ConatinerLinkBtn'>
                     <Button variant="contained" className="limit-input" type="submit">Cadastrar</Button>
                     <p>Voltar pra <Link className='RegisterLink' to={"/"}><span>Home</span></Link></p>
